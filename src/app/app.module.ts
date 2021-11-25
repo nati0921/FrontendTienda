ade2695fd53d2e51a4aed0dc9fd1c358635bd406
John DoeAndresVillamil <bcl.evillamil@gmail.com>
2021-11-24 22:19:56 -0500

    Revert "Creación plantillas"
    This reverts commit 1d6ad83a24ae49b9c8cc2e5361039dfbb34975fd.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
-import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
-import { InicioComponent } from './plantilla/inicio/inicio.component';
-import { ErrorComponent } from './plantilla/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BarraNavegacionComponent,
-    PiePaginaComponent,
-    InicioComponent,
-    ErrorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
