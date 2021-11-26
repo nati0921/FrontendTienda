b895a0d8dd7529628d8c42b9fd3d7f93da888f0c
nati0921 <55897435+nati0921@users.noreply.github.com>
2021-11-26 16:26:23 -0500

    Rutas persona y producto

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,11 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarPersonasComponent } from './personas/buscar-personas/buscar-personas.component';
 import { CrearPersonasComponent } from './personas/crear-personas/crear-personas.component';
+import { EditarPersonasComponent } from './personas/editar-personas/editar-personas.component';
+import { EliminarPersonasComponent } from './personas/eliminar-personas/eliminar-personas.component';
+import { BuscarProductosComponent } from './productos/buscar-productos/buscar-productos.component';
+import { CrearProductosComponent } from './productos/crear-productos/crear-productos.component';
+import { EditarProductosComponent } from './productos/editar-productos/editar-productos.component';
+import { EliminarProductosComponent } from './productos/eliminar-productos/eliminar-productos.component';
 
 const routes: Routes = [
   {
-    path: "crearpersonas",
+    path: "personas/buscarpersonas",
+    component : BuscarPersonasComponent
+  },
+  {
+    path: "personas/crearpersonas",
     component : CrearPersonasComponent
+  },
+  {
+    path: "personas/editarpersonas",
+    component : EditarPersonasComponent
+  },  
+  {
+    path: "personas/eliminarpersonas",
+    component : EliminarPersonasComponent
+  },
+  {
+    path: "productos/buscarproductos",
+    component : BuscarProductosComponent
+  },
+  {
+    path: "productos/crearproductos",
+    component : CrearProductosComponent
+  },
+  {
+    path: "productos/editarproductos",
+    component : EditarProductosComponent
+  },
+  {
+    path: "productos/eliminarproductos",
+    component : EliminarProductosComponent
   }
 ];
 
--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CambiarClaveComponent } from './cambiar-clave/cambiar-clave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "cambiarclave",
+    component : CambiarClaveComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
