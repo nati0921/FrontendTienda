34c9bc116946eaf500d5a7c296308f00668ad6ac
AndresVillamil <bcl.evillamil@gmail.com>
2021-11-26 14:32:27 -0500

    Páginas y rutas Cliente

--- src/app/modulos/cliente/cliente-routing.module.ts
+++ src/app/modulos/cliente/cliente-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EnviarSolicitudesComponent } from './enviar-solicitudes/enviar-solicitudes.component';
+import { RealizarPedidoComponent } from './realizar-pedido/realizar-pedido.component';
+import { VerificarSolicitudesComponent } from './verificar-solicitudes/verificar-solicitudes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "enviarsolicitudes",
+  component: EnviarSolicitudesComponent
+  },
+  {
+    path: "verificarsolicitudes",
+    component: VerificarSolicitudesComponent
+  },
+  {
+    path: "realizarpedido",
+    component: RealizarPedidoComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
