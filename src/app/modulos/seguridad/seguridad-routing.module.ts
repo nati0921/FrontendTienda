cb19c3e0daedd4ac84ed29ee41de9e665dba1b60
nati0921 <55897435+nati0921@users.noreply.github.com>
2021-11-26 21:49:33 -0500

    Route seguridad

--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CambiarClaveComponent } from './cambiar-clave/cambiar-clave.component';
+import { IdentificarComponent } from './identificar/identificar.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
 const routes: Routes = [
   {
     path: "cambiarclave",
     component : CambiarClaveComponent
+  },
+  {
+    path: "identificar",
+    component : IdentificarComponent
+  },
+  {
+    path: "recuperarclave",
+    component : RecuperarClaveComponent
   }
 ];
 
