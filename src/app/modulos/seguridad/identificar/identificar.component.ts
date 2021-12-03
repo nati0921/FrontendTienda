56d9ad73517faa5db265e66bb74c0a6c6210516b
AndresVillamil <bcl.evillamil@gmail.com>
2021-12-03 18:20:03 -0500

    Inicio

--- src/app/modulos/seguridad/identificar/identificar.component.ts
+++ src/app/modulos/seguridad/identificar/identificar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-identificar',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentificarComponent implements OnInit {
 
-  constructor() { }
+  fgValidador : FormGroup = this.fb.group({
+    'usuario': ['', [Validators.required, Validators.email]],
+    'clave': ['',[Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    
+  }
+
+  IdentificarUsuario(){
+    let usuario = this.fgValidador.controls['usuario'].value;
+    let clave = this.fgValidador.controls['clave'].value;
+    alert(usuario)
+    alert(clave)
   }
 
 }
